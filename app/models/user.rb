4ca5e7eb9ee27b4bb16833e2a9042ba4b1c69541
Alexander Le <alextle0125@gmail.com>
2014-08-29 16:08:08 -0400

    initial commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,11 @@ class User < ActiveRecord::Base
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
   validates :password, :length => {minimum: 4}
-  
+
 
   has_many :tagations
   has_many :tags, :through => :tagations
+  has_many :scores
   # validates :topics, presence: true
   # validates :comments, presence: true
 
--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,3 @@ require 'bcrypt'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-test:
-  adapter: sqlite3
-  database: ":memory:"
-  timeout: 500
