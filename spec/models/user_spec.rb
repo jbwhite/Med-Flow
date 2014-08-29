6ab579eb28a35375636db348cfaff8897b022bf9
Stephen Estrada <stcrestrada@gmail.com>
2014-08-29 13:08:31 -0400

    added rpsec test to validates that username equals the user input test passed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,2 +1,11 @@
 require 'rails_helper'
 
+describe User do 
+	let!(:user) { User.create(username: "Justin", email: "jwag89@gmail.com", password:"jwag") }
+
+	context "username" do
+		it "should return the username" do  
+			expect(user.username).to eq("Justin")
+		end
+	end
+end
