20bcf931cb788f6dafec213388d33243b132d690
Joseph White <josephbarrywhite@gmail.com>
2014-08-28 22:55:52 -0400

    updated seed file to add tags and tagations, removed validations from user

--- Preventive-Measures/app/models/user.rb
+++ Preventive-Measures/app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
 
   has_many :topics
   has_many :comments
-  validates :topics, presence: true
-  validates :comments, presence: true
+  has_many :tagations
+  has_many :tags, :through => :tagations
+  # validates :topics, presence: true
+  # validates :comments, presence: true
 end
