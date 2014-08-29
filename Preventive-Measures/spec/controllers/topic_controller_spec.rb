f5872398e16814dfabc7d2045eb25caceec08b34
Stephen Estrada <stcrestrada@gmail.com>
2014-08-28 21:04:30 -0400

    create migration for topics.

--- Preventive-Measures/spec/controllers/topic_controller_spec.rb
+++ Preventive-Measures/spec/controllers/topic_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe TopicController, :type => :controller do
+describe TopicsController do 
+	let!(:topic) { Topic.create(title: "Acne", body:"How I was able to get rid of acne was a miracle." )}
+
+	describe "GET #index" do 
+		it "assigns all topics  as @topics"
+		get :index
+		expect(assigns(:topics)).to eq(Topic.all)
+	end
 
 end
