e4f59bdc98ac06c8aed49e5f57f0902b86b8556b
Stephen Estrada <stcrestrada@gmail.com>
2014-08-29 11:16:59 -0400

    add spec test for comment model add comment datatype body

--- Preventive-Measures/spec/models/comment_spec.rb
+++ Preventive-Measures/spec/models/comment_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 describe Comment do
-	let(:comment) { Comment.create(body: "Hello how was your day") }
+	let!(:user) { User.create(username: "Justin W", email: "jwag89@gmail.com", password:"jwag") }
+	let!(:topic) { Topic.create(title:"Forget DBC", body:"I am leaving in 2 weeks", user_id: 1) }
+	let!(:comment) { Comment.create(body: "Hello how was your day", user_id: 1, topic_id: 1) }
 	
 	context "body" do
 		it "should return the body" do  
+			p comment
 			expect(comment.body).to eq("Hello how was your day")
 		end
 	end
