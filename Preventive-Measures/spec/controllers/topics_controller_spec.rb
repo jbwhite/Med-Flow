d87a9fc3e0596875e3d44cd91766f14742685a3f
Stephen Estrada <stcrestrada@gmail.com>
2014-08-28 21:48:57 -0400

    created show action in controller that will render each topic to show page created show view passed show controller in rspec

--- Preventive-Measures/spec/controllers/topics_controller_spec.rb
+++ Preventive-Measures/spec/controllers/topics_controller_spec.rb
@@ -4,9 +4,16 @@ describe TopicsController do
 	let!(:topic) { Topic.create(title: "Acne", body:"How I was able to get rid of acne was a miracle." )}
 
 	describe "GET #index" do 
-		it "assigns all topics  as @topics" do
+		it "assigns all topics as @topics" do
 			get :index
 			expect(assigns(:topics)).to eq(Topic.all)
+		end
+	end
+
+	describe "#GET show" do 
+		it "assigns the requested topic as @topic" do 
+			get :show, { id: topic.to_param }
+			expect(assigns(:topic)).to eq(topic)
 		end
 	end
 end
