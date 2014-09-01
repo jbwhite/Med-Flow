88edfdd5f0d5c009ac0d8a79baebbf2211609129
Justin Wagner <jwag789@gmail.com>
2014-08-31 20:50:33 -0400

    Add voting limitation for comment and scores

--- app/controllers/comment_scores_controller.rb
+++ app/controllers/comment_scores_controller.rb
@@ -6,9 +6,15 @@ class CommentScoresController < ApplicationController
 
   def create
     @user = User.find(session[:user_id])
-    @comment_score = @user.comment_scores.create(comment_score_params)
-    if @comment_score.save
-      redirect_to root_path
+    votes = @user.comment_scores.map {|score| score.comment.id }
+    comment = comment_score_params[:comment_id]
+    unless votes.include?(comment.to_i)
+      @comment_score = @user.comment_scores.create(comment_score_params)
+      if @comment_score.save
+        redirect_to root_path
+      else
+        redirect_to root_path
+      end
     else
       redirect_to root_path
     end
